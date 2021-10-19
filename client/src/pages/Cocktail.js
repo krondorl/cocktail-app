13e709e304b6bf2cdbf061f80202717710153630
Burucs Adam <burucs.adam@nexius.hu>
2021-10-19 15:32:24 +0200

    Add content to pages

--- client/src/pages/Cocktail.js
+++ client/src/pages/Cocktail.js
@@ -4,8 +4,13 @@ class Cocktail extends Component {
   render() {
     return (
     <div className="App">
-      <h1>Project Home</h1>
-      
+      <h1>Cocktail App</h1>
+      <div>
+        <button onClick={this.random}>New random</button>
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
+        <button onClick={this.search}>Search</button>
+      </div>
+      <div></div>
     </div>
     );
   }
--- client/src/pages/Intro.js
+++ client/src/pages/Intro.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Intro extends Component {
   render() {
     return (
     <div className="App">
-      <h1>Project Home</h1>
-      
+      <h1>Welcome to Cocktail App</h1>
+      <div>
+        <Link to="/cocktail">Enter</Link>
+      </div>
     </div>
     );
   }
